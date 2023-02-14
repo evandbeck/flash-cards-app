76ca142d0e231d602ef2129a8493d438105129a3
Evan Beck <evandrydenbeck@gmail.com>
2023-02-14 11:22:13 -0500

    added User model and Sessions controller

--- flash-cards-back/config/initializers/session_store.rb
+++ flash-cards-back/config/initializers/session_store.rb
@@ -2,3 +2,4 @@ if Rails.env === 'production'
     Rails.application.config.session_store :cookie_store, key: '_flash-cards', domain: 'flash-cards-json-api'
   else
     Rails.application.config.session_store :cookie_store, key: '_flash-cards'
+  end
--- flash-cards-back/config/routes.rb
+++ flash-cards-back/config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+
+  post '/login',    to: 'sessions#create'
+  post '/logout',   to: 'sessions#destroy'
+  get '/logged_in', to: 'sessions#is_logged_in?'
+
+  resources :users, only: [:create, :show, :index] do
+    resources :items, only: [:create, :show, :index, :destroy]
+  end
+  
+  # post '/users',         to: 'users#create'
+  # get '/users/:user_id', to: 'users#show'
+  # get '/users',          to: 'users#index'
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
