5d8fab7d212a9b86382c93ae5bc8272555a568e6
Evan Beck <evandrydenbeck@gmail.com>
2023-02-20 10:10:14 -0500

    Signup.jsx logic

--- flash-cards-app/client/src/Signup.jsx
+++ flash-cards-app/client/src/Signup.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function Signup() {
-  return (
-    <div>Signup</div>
-  )
-}
+class Signup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      username: '',
+      email: '',
+      password: '',
+      password_confirmation: '',
+      errors: ''
+     };
+  }
 
-export default Signup
+    handleChange = (event) => {
+        const {name, value} = event.target
+        this.setState({
+        [name]: value
+        })
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+    };
+    
+    render() {
+        const {username, email, password, password_confirmation} = this.state
+        return (
+            <div>
+                <h1>Sign Up</h1>        
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        placeholder="username"
+                        type="text"
+                        name="username"
+                        value={username}
+                        onChange={this.handleChange}
+                    />
+                    <input
+                        placeholder="email"
+                        type="text"
+                        name="email"
+                        value={email}
+                        onChange={this.handleChange}
+                    />
+                    <input 
+                        placeholder="password"
+                        type="password"
+                        name="password"
+                        value={password}
+                        onChange={this.handleChange}
+                    />          
+                    <input
+                        placeholder="password confirmation"
+                        type="password"
+                        name="password_confirmation"
+                        value={password_confirmation}
+                        onChange={this.handleChange}
+                    />
+                    <button placeholder="submit" type="submit">Sign Up</button>
+                </form>
+            </div>
+            );
+        };
+};
+
+export default Signup;
