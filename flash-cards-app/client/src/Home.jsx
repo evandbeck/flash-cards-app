0a858b786c93e8e48f2dcaaa5564fdfc123f23a3
Evan Beck <evandrydenbeck@gmail.com>
2023-02-28 15:21:07 -0500

    styling test

--- flash-cards-app/client/src/Home.jsx
+++ flash-cards-app/client/src/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function Home() {
   return (
-    <div>
+    <div className="HomePadding">
         <Link to='/login'>Log In</Link>
         <br></br>
         <Link to='/signup'>Sign Up</Link>
