b6f6592d3d8ace6ed4a4e6318d9c2662bd130317
Evan Beck <evandrydenbeck@gmail.com>
2023-03-30 10:31:17 -0400

    add controllers

---
+++ flash-cards-app/app/controllers/api/v1/cards_controller.rb
@@ -0,0 +1,6 @@
+module Api
+    module V1
+        class CardsController < ApplicationController
+        end
+    end
+end
---
+++ flash-cards-app/app/controllers/api/v1/tags_controller.rb
@@ -0,0 +1,6 @@
+module Api
+    module V1
+        class TagsController < ApplicationController
+        end
+    end
+end
